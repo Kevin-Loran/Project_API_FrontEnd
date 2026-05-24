afdac181f3aa74a06bffc9d503e9f086904aa781
Kevin-Loran <kevinloran.eng@gmail.com>
2026-05-24 00:06:26 -0300

    feat: API_REST_FRONTEND/Fazendo o registro de usuarios.

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator'
+import { get } from 'lodash';
 
 import { Conteiner } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
 
-export default function register(){
+export default function Register() {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let formErrors = false;
+
+        if (nome.length < 3 || nome.length > 255) {
+            formErrors = true;
+            toast.error('Nome deve ter entre 3 e 255 caacteres');
+        }
+        
+        if (!isEmail(email)) {
+            formErrors = true;
+            toast.error('E-mail inválido.');
+        }
+        
+        if (password.length < 3 || password.length > 50) {
+            formErrors = true;
+            toast.error('Senha deve ter entre 6 e 50 caacteres');
+        }
+
+        if (formErrors) return;
+
+        try {
+            await axios.post('/users/', {
+                nome,
+                password,
+                email,
+            });
+
+            toast.success('Voce fez seu cadastro');
+            history.push('/login');
+        } catch(e) {
+            const errors = get(e, 'response.data.errors', []);
+
+            errors.map(error => toast.error(error));
+        }
+
+    }
+
     return (
         <Conteiner>
-          <h1>register</h1>
+          <h1>Crie sua conta</h1>
+
+        <Form onSubmit={handleSubmit}>
+          <label htmlFor='nome'>
+            Nome:
+            <input 
+             type='text'
+             value={nome} 
+             onChange={e => setNome(e.target.value)}
+             placeholder='Seu nome'
+             />
+          </label>    
+
+          <label htmlFor='nome'>
+            E-mail:
+            <input 
+             type='email'
+             value={email} 
+             onChange={e => setEmail(e.target.value)}
+             placeholder='Seu e=mail'
+             />
+          </label>    
+
+          <label htmlFor='nome'>
+            senha:
+            <input 
+             type='password'
+             value={password} 
+             onChange={e => setPassword(e.target.value)}
+             placeholder='Sua senha'
+             />
+          </label>    
+          
+          <button type='submit'>Criar minha conta</button>
+        </ Form>
       </ Conteiner>
     )
 }
